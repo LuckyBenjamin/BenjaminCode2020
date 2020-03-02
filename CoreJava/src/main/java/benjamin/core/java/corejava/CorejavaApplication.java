f88847fd95bd903c221ccd4a8ce761e1e3b54351
lizhibin <lizhibin_code@163.com>
2020-03-03 00:45:42 +0800

    Initial commit

---
+++ CoreJava/src/main/java/benjamin/core/java/corejava/CorejavaApplication.java
@@ -0,0 +1,13 @@
+package benjamin.core.java.corejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorejavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorejavaApplication.class, args);
+    }
+
+}
