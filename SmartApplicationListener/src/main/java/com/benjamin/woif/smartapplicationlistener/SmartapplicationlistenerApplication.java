192b8962573e7307fbd471c9ecd53ce5d3a1061c
lizhibin <lizhibin_code@163.com>
2020-07-04 00:09:19 +0800

    创建工程

---
+++ SmartApplicationListener/src/main/java/com/benjamin/woif/smartapplicationlistener/SmartapplicationlistenerApplication.java
@@ -0,0 +1,13 @@
+package com.benjamin.woif.smartapplicationlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartapplicationlistenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartapplicationlistenerApplication.class, args);
+    }
+
+}
---
+++ SmartApplicationListener/src/test/java/com/benjamin/woif/smartapplicationlistener/SmartapplicationlistenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benjamin.woif.smartapplicationlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartapplicationlistenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
